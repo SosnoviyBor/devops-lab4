2c7ae8b230795d5842dc652cc7fff5659f34bc6b
Sosnoviy Bor <59932328+SosnoviyBor@users.noreply.github.com>
2022-12-07 11:52:24 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0'
 
 const app = express()
 app.get('/', (request, response) => {
-  response.send('<h2 style="color: red"> Hello world? I guess?<h2>');
+  response.send('<h2 style="color: red"> Hello world? I guess?<h2>')
 })
 
 app.listen(PORT, HOST)
