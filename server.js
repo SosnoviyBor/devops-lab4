34719a6d31ca3c281bb1c753b002d9cddb31b487
Sosnoviy Bor <59932328+SosnoviyBor@users.noreply.github.com>
2022-12-07 11:49:30 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
-const express = require('express');
+const express = require('express')
 
-const PORT = 80;
-const HOST = '0.0.0.0';
+const PORT = 80
+const HOST = '0.0.0.0'
 
-const app = express();
+const app = express()
 app.get('/', (request, response) => {
   response.send('<h2 style="color: red"> Hello world? I guess?<h2>');
-});
+})
 
-app.listen(PORT, HOST);
-console.log(`Yup, im runnin' on http://${HOST}:${PORT}`);
+app.listen(PORT, HOST)
+console.log(`Yup, im runnin' on http://${HOST}:${PORT}`)
